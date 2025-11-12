204c2a3163f60df1141174e471ef0e642f585f23
nikita <dayzwaitik123@yandex.ru>
2025-11-12 23:59:27 +0300

    репозиторий пользователей

---
+++ internal/storage/repositories/user/user.go
@@ -0,0 +1,26 @@
+package repo_user
+
+import (
+	"log/slog"
+
+	"github.com/sariya23/manage_pr_service/internal/storage/database"
+)
+
+const (
+	UserTableUserIDField   = "user_id"
+	UserTableUsernameField = "username"
+	UserTableIsActiveField = "is_active"
+	UserTableName          = "user"
+)
+
+type UserRepository struct {
+	conn *database.Database
+	log  *slog.Logger
+}
+
+func NewUserRepository(conn *database.Database, log *slog.Logger) *UserRepository {
+	return &UserRepository{
+		conn: conn,
+		log:  log,
+	}
+}
