5471088921455e49ac0c361b65bc446bd3d72e5a
nikita <dayzwaitik123@yandex.ru>
2025-11-14 23:06:36 +0300

    MergePullRequest

--- internal/storage/repositories/pull_request/merge.go
+++ internal/storage/repositories/pull_request/merge.go
@@ -2,15 +2,34 @@ package repo_pull_request
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/sariya23/manage_pr_service/internal/converters"
 	"github.com/sariya23/manage_pr_service/internal/models/domain"
+	"github.com/sariya23/manage_pr_service/internal/models/dto"
 )
 
 func (r *PullRequestRepository) MergePullRequest(ctx context.Context, prID string) (*domain.PullRequest, error) {
-	const operation = "storage.repositories.pull_request.MergePullRequest"
+	const operationPlace = "storage.repositories.pull_request.MergePullRequest"
 
-	//	updatePullRequestSQL := `update pull_request set status='MERGED' where pull_request_id = $1
-	//returning pull_request_id, pull_request_name, author_id, status, merged_at, created_at`
+	updatePullRequestSQL := `update pull_request set status='MERGED' where pull_request_id = $1
+	returning pull_request_id, pull_request_name, author_id, status, merged_at, created_at, assigned_reviewers`
 
-	panic("implement me")
+	row := r.conn.GetPool().QueryRow(ctx, updatePullRequestSQL, prID)
+	var pullRequest dto.PullRequestDB
+
+	err := row.Scan(
+		&pullRequest.ID,
+		&pullRequest.Name,
+		&pullRequest.AuthorID,
+		&pullRequest.Status,
+		&pullRequest.MergedAt,
+		&pullRequest.CreatedAt,
+		&pullRequest.AssignedReviewerIDs)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", operationPlace, err)
+	}
+
+	res := converters.PullRequestDBToDomain(pullRequest)
+	return &res, nil
 }
