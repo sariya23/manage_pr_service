e4ef13c4fd69530f44a538a753304955f3a9f667
nikita <dayzwaitik123@yandex.ru>
2025-11-15 14:31:12 +0300

    PullRequest

---
+++ tests/factory/pull_request.go
@@ -0,0 +1,13 @@
+package factory
+
+import "time"
+
+type PullRequest struct {
+	ID                  string
+	Name                string
+	AuthorID            string
+	Status              string
+	MergedAt            *time.Time
+	CreatedAt           time.Time
+	AssignedReviewerIDs []string
+}
