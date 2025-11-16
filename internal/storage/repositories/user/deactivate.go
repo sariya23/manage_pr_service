d69b0f92e731516914f0460edf12359d0482a0c7
nikita <dayzwaitik123@yandex.ru>
2025-11-16 22:55:29 +0300

    reanme

--- internal/storage/repositories/user/deactivate.go
+++ internal/storage/repositories/user/deactivate.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 )
 
-func (r *UserRepository) MultiDeactivate(ctx context.Context, userIDs []string) error {
+func (u *UserRepository) MultiDeactivate(ctx context.Context, userIDs []string) error {
 	const operationPlace = "storage.repositories.user.MultiDeactivate"
 
 	deactivateQuery := `update "user" set is_active=false where user_id=any($1)`
 
-	_, err := r.conn.GetPool().Exec(ctx, deactivateQuery, userIDs)
+	_, err := u.conn.GetPool().Exec(ctx, deactivateQuery, userIDs)
 
 	if err != nil {
 		return fmt.Errorf("%s: %w", operationPlace, err)
